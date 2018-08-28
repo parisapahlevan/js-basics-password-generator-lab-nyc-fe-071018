aa06466f85082af334fd8dd71d9fc82c018d0b3b
parisa pahlevan <paris.pahlevan@gmail.com>
2018-08-28 22:15:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 window.addEventListener("load", () => {
   //start your code here
+  document.addEventListener("submit", (event) => {
+    event.preventDefault()
+    const input = document.getElementById("passwordlength")
+    let passwordLength = input.value 
+    let lowercaseCharacters = "glsvdnlgslljjhjh".split("")
+    
+  })
 });
