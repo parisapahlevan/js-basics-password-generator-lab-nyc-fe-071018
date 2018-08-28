4325734232e60ecafee11b89b4b62ce5ab5b991a
parisa pahlevan <paris.pahlevan@gmail.com>
2018-08-28 22:23:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 window.addEventListener("load", () => {
   //start your code here
   let outputResult = document.querySelector("output")
-  let loweCaseCharacters = ""
+  let loweCaseCharacters = "abcdefghijklmnopqrstuvwxyz".split('')
+ 
   document.addEventListener("submit", (event) => {
     event.preventDefault()
     let input = document.getElementById("passwordlength")
     let passwordLength = input.value 
-    let lowercaseCharacters = "glsvdnlgslljjhjh".split("")
+    
     
   })
 });
