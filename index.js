167cb696f4da27e1fa1689824c8271be4ee7bff9
parisa pahlevan <paris.pahlevan@gmail.com>
2018-08-28 22:21:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 window.addEventListener("load", () => {
   //start your code here
   let outputResult = document.querySelector("output")
-
+  let loweCaseCharacters = ""
   document.addEventListener("submit", (event) => {
     event.preventDefault()
     let input = document.getElementById("passwordlength")
